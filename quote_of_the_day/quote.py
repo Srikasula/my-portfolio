869b9344e20c6683272c91706c8cc13c7ed2f1e3
Srikasula <srikanthgoudk3@gmail.com>
2025-05-24 11:37:55 -0500

    Create quote.py

---
+++ quote_of_the_day/quote.py
@@ -0,0 +1,14 @@
+# quote_of_the_day/quote.py
+
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Every day is a second chance.",
+    "You are stronger than you think.",
+    "Never give up on your dreams.",
+    "Push yourself, because no one else is going to do it for you."
+]
+
+print("🌟 Quote of the Day 🌟")
+print(random.choice(quotes))
